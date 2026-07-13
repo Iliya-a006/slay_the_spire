226bad6dde66513eedc3ef7184391231610fbc97
Hamed Etehad Tavakol <het39962120@gmail.com>
2026-07-13 22:59:19 +0330

    begin of player & card & debug on mainmenu

---
+++ cardimagemanager.cpp
@@ -0,0 +1,3 @@
+#include "cardimagemanager.h"
+
+cardimagemanager::cardimagemanager() {}
---
+++ cardimagemanager.h
@@ -0,0 +1,12 @@
+#ifndef CARDIMAGEMANAGER_H
+#define CARDIMAGEMANAGER_H
+
+#include <QObject>
+
+class cardimagemanager
+{
+public:
+    cardimagemanager();
+};
+
+#endif // CARDIMAGEMANAGER_H
